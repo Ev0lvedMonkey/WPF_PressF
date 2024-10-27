75ec4aa41190ab354cb358af9d8a678b5f659137
Ev0lvedMonkey <135731224+Amir-safarov@users.noreply.github.com>
2024-10-27 18:28:32 +0300

    commit 2.3

---
+++ WpfApp1/Pages/AuthRegPages/RegNewEqupFailure.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Database;
+
+namespace WpfApp1.Pages.AuthRegPages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegNewEqupFailure.xaml
+    /// </summary>
+    public partial class RegNewEqupFailure : Page
+    {
+        public RegNewEqupFailure()
+        {
+            InitializeComponent();
+            EqupCB.ItemsSource = App.DB.Equipments.ToList();
+            EqupCB.DataContext = "EquipName";
+            ReasonCB.ItemsSource = App.DB.FailureReasons.ToList();
+            ReasonCB.DataContext = "ReasonName";
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if (CheckTextFields(stringBuilder))
+            {
+                MessageBox.Show(stringBuilder.ToString());
+                stringBuilder.Clear();
+                return;
+            }
+            else
+            {
+                EquipmentFailures equipmentFailures = new EquipmentFailures
+                {
+                    EquipmentID = (EqupCB.SelectedItem as Equipments).EquipmentID,
+                    FailureReasonID = (EqupCB.SelectedItem as FailureReasons).FailureReasonID,
+                    FailureDate = (DateTime)FailureDateDP.SelectedDate,
+                    MasterID = App.CurrentUser.UserID,
+                    FixDate = null
+                };
+                App.DB.EquipmentFailures.Add(equipmentFailures);
+                App.DB.SaveChanges();
+                NavigationService.Navigate(new RegNewEqupFailure());
+            }
+        }
+
+        private bool CheckTextFields(StringBuilder stringBuilder)
+        {
+            if (ReasonCB.SelectedItem == null)
+                stringBuilder.AppendLine("Выберите причину поломки");
+            if (EqupCB.SelectedItem == null)
+                stringBuilder.AppendLine("Выберите оборудование");
+            if (FailureDateDP.SelectedDate == null)
+                stringBuilder.AppendLine("Выберите время поломки");
+            if (stringBuilder.Length > 0)
+                return true;
+            else return false;
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegNewEqupFailure());
+        }
+    }
+}
---
+++ WpfApp1/Pages/Lists/EquipmentFailuresList.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Database;
+using WpfApp1.Pages.AuthRegPages;
+
+namespace WpfApp1.Pages.Lists
+{
+    /// <summary>
+    /// Логика взаимодействия для EquipmentFailuresList.xaml
+    /// </summary>
+    public partial class EquipmentFailuresList : Page
+    {
+        public EquipmentFailuresList()
+        {
+            InitializeComponent();
+            EquipFailuresList.DataContext = App.DB.EquipmentFailures.Where(x => x.MasterID == App.CurrentUser.UserID).ToList();
+            DataContext = App.DB.EquipmentFailures.Where(x => x.MasterID == App.CurrentUser.UserID).ToList(); 
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(App.GetRightPage());
+        }
+
+        private void FixDateDP_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is DatePicker fixDateDP)
+            {
+                if (EquipFailuresList.SelectedItem is EquipmentFailures selectedFailure)
+                {
+                    selectedFailure.FixDate = fixDateDP.SelectedDate;
+                    App.DB.SaveChanges();
+                }
+                else
+                {
+                    MessageBox.Show($"{EquipFailuresList.SelectedItem}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sender is not a DatePicker.");
+            }
+        }
+
+        private void RegNewEqupFailureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegNewEqupFailure());
+        }
+    }
+}
